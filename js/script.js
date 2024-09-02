fc0c9793b5945c866c788b78541d54c448b23951
ramiro bartolotti <ramiroemanuel083@gmail.com>
2024-09-02 17:20:16 -0300

    primer commit, juego de numeros

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//inicio programa
+const min = 1;
+const max = 10;
+
+//array con las variables posibles
+const numeros = [];
+for (let i = min; i <= max; i++) {
+    numeros.push(i);
+}
+
+//eleccion de num aleatorio del array
+const randomIndex = Math.floor(Math.random() * numeros.length);
+const numeroRandom = numeros[randomIndex];
+
+//pido al usuario que eliga el num
+let userGuess = parseInt(prompt(`Adivina el número entre ${min} y ${max}:`));
+
+//vemos si el num ingresado es correcto
+if (userGuess === numeroRandom) {
+    alert('¡Felicidades! Adivinaste el número.');
+} else {
+    alert(`Lo siento, el número correcto era ${numeroRandom}.`);
+}
+
+//preguntamos si quiere jugar de nuevo
+const jugarOtravez = confirm('¿Quieres jugar de nuevo?');
+
+if (jugarOtravez) {
+    //si quiere jugar denuevo refrescamos la pagina
+    location.reload();
+} else {
+    //sino quiere jugar de nuevo lo despedimos
+    alert('Gracias por jugar. ¡Hasta luego!');
+}
